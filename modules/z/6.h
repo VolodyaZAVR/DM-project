afd5ce35352018019d9f89bfdd6ae8c474f6f733
Andrey <29797602+kogutenko@users.noreply.github.com>
2020-04-15 16:45:03 +0300

    Revert "Update 6.h"

--- modules/z/6.h
+++ modules/z/6.h
@@ -1,38 +1,35 @@
 integer *Z6(integer *A, integer *B)
 {
     integer *R;
-    natural *aN, *bN;
+    natural *aN, *bN, A1, B1;
     aN = Z1(A);
     bN = Z1(B);
-    R = init_integer(aN -> length);
-    R = init_integer(bN -> length);
-    R = NULL;
 
     if (Z2(A) == 2 && Z2(B) == 2) {
-        R = Z4(N4(aN, bN));
+        R = N4(A, B);
         R -> sign = true;
     }
     else if (Z2(A) == 1 && Z2(B) == 1) {
-        R = Z4(N4(aN, bN));
+        R = N4(aN, bN);
         R -> sign = false;
     }
     else if (Z2(A) == 2 && Z2(B) == 1) {
-        if ((N1(aN, bN) == 2) || (N1(aN, bN) == 0)) {
-            R = Z4(N5(aN, bN));
+        if (N1(aN, bN) == 2) {
+            R = N5(A, bN);
             R -> sign = true;
         }
         else if (N1(aN, bN) == 1) {
-            R = Z4(N5(bN, aN));
+            R = N5(bN, A);
             R -> sign = false;
         }
     }
     else if (Z2(A) == 1 && Z2(B) == 2) {
         if (N1(aN, bN) == 2) {
-            R = Z4(N5(aN, bN));
+            R = N5(aN, B);
             R -> sign = false;
         }
-        else if ((N1(aN, bN) == 1) || (N1(aN, bN) == 0)) {
-            R = Z4(N5(bN, aN));
+        else if (N1(aN, bN) == 1) {
+            R = N5(B, aN);
             R -> sign = true;
         }
     }
@@ -40,20 +37,18 @@ integer *Z6(integer *A, integer *B)
         if (Z2(B) == 2) {
             B -> sign = true;
         }
-        else if (Z2(B) == 1) {
+        else if (Z2(B) == 2) {
             B -> sign = false;
         }
-        R = init_integer(B -> length);
         R = B;
     }
-    else if (Z2(B) == 0) {
+    else if (Z1(B) == 0) {
         if (Z2(A) == 2) {
             A -> sign = true;
         }
-        else if (Z2(A) == 1) {
+        else if (Z2(B) == 2) {
             A -> sign = false;
         }
-        R = init_integer(A -> length);
         R = A;
     }
     return(R);
