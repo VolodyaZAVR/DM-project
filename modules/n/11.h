2e03b6f3edf0d83758c45eb18b7f14e5a1fa9a5b
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-26 15:00:08 +0300

    Update 11.h

--- modules/n/11.h
+++ modules/n/11.h
@@ -45,9 +45,10 @@ natural *N11(natural *A, natural *B)
 
     while(i--)
     {
-        N -> length += 1;
+    	if((N -> length != 1) || (N -> digits[0] != 0))
+		    N -> length += 1;
         resize_natural(N, N -> length);
-
+        
         for(b = (N -> length); b >= 2; b--)
             N -> digits[b - 1] = (N -> digits[b - 2]);
 
@@ -59,14 +60,15 @@ natural *N11(natural *A, natural *B)
         R -> digits[c++] = t;
 
         N = N9(N, D, t);
-        if((N -> length == 1) && (N -> digits[0] == 0))
-            N -> length = 0;
 
         C -> length -= 1;
     }
-
+    
+    free_natural(C);
+    free_natural(D);
     free_natural(N);
-    R -> length = c;
+    
+	R -> length = c;
 
     for(b = 0; b < c/2; b++)
     {
