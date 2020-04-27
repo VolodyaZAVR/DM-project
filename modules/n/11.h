0a7aa906162b26b496255a59675cafb72155d04f
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-27 19:54:57 +0300

    Update 11.h

--- modules/n/11.h
+++ modules/n/11.h
@@ -2,12 +2,12 @@
 
 natural *N11(natural *A, natural *B)
 {
-    natural *C, *D, *N, *R;
+    natural *C, *D, *N, *R, *G;
     size_t i, c = 0;
     signed char t = 0;
-    size_t u;
-
-    N = init_natural((B -> length) + 1);
+    size_t u = 0;
+    
+    N = init_natural(B -> length);
     R = init_natural(A -> length);
     C = copy_natural(A);
     D = copy_natural(B);
@@ -24,6 +24,7 @@ natural *N11(natural *A, natural *B)
     if(N1(N, D) == 1)
     {
         N -> length += 1;
+        resize_natural(N, N -> length);
         
         for(b = (N -> length) - 1; b >= 1; b--)
             N -> digits[b] = (N -> digits[b - 1]);
@@ -34,41 +35,52 @@ natural *N11(natural *A, natural *B)
     }
 
     i = (C -> length);
-
+    
     t = N10(N, D);
     R -> digits[c++] = t;
     R -> length = 1;
-
-    N = N9(N, D, t);
-    if((N -> length == 1) && (N -> digits[0] == 0))
-        N -> length = 0;
+    
+    G = init_natural(N -> length);
+    G = N9(N, D, t);
+    free_natural(N);
+    
+    if((G -> length == 1) && (G -> digits[0] == 0)){
+    	G -> length = 0;
+    	resize_natural(G, 1);
+	}
 
     while(i--)
     {
-    	if((N -> length != 1) || (N -> digits[0] != 0))
-		    N -> length += 1;
-        resize_natural(N, N -> length);
+    	if((G -> length != 1) || (G -> digits[0] != 0))
+		    {
+		    	G -> length += 1;
+                resize_natural(G, G -> length);
+			}
         
-        for(b = (N -> length); b >= 2; b--)
-            N -> digits[b - 1] = (N -> digits[b - 2]);
+        for(b = (G -> length); b >= 2; b--)
+            G -> digits[b - 1] = (G -> digits[b - 2]);
 
-        N -> digits[0] = (C -> digits[u - 1]);
-        if(u != 0)
+        G -> digits[0] = (C -> digits[u - 1]);
+        N = copy_natural(G);
+        
+		if(u != 0)
             u--;
 
         t = N10(N, D);
         R -> digits[c++] = t;
 
-        N = N9(N, D, t);
-
+        G = N9(N, D, t);
+        free_natural(N);
+        
         C -> length -= 1;
     }
     
     free_natural(C);
     free_natural(D);
-    free_natural(N);
+    free_natural(G);
     
 	R -> length = c;
+	resize_natural(R, c);
 
     for(b = 0; b < c/2; b++)
     {
