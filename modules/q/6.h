f55c0bf3a91e60c6810be9e7c65c4a25820178ed
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-19 15:55:28 +0300

    Q-6 updated

--- modules/q/6.h
+++ modules/q/6.h
@@ -1,35 +1,32 @@
-//Бессонов Никита 9305
-//Вычитание дробей
+// Бессонов Никита 9305
+// Вычитание дробей
+
 fraction *Q6(fraction *A, fraction *B) {
-    fraction *R = NULL, *R1 = NULL;
-    natural *N = NULL;
-    integer *C = NULL, *D = NULL, *ResChisl = NULL, *tempA = NULL, *tempB = NULL;
-
-    tempA = Z4(A->denominator);
-    tempB = Z4(B->denominator);
-    N = N8(A->denominator, B->denominator);//новый знаменатель
-    C = Z8(A->numerator, tempB);//новый числитель первой дроби
-    D = Z8(B->numerator, tempA);//новый числитель второй дроби
-    ResChisl = Z7(C, D);// вычитание новых числителей
-
-
-    R = init_fraction(ResChisl, N); //составление новой дроби
-    R1 = Q1(R); // сокращение новой дроби
-
-    //очистка
-    free_natural(N);
-    N = NULL;
-    free_integer(ResChisl);
-    ResChisl = NULL;
-    free_integer(C);
-    C = NULL;
-    free_integer(D);
-    D = NULL;
-    free_fraction(R);
-    R = NULL;
-    free_integer(tempA);
-    free_integer(tempB);
-
-    return R1;
+    
+    integer *A_int_denom = Z4(A -> denominator);
+    integer *B_int_denom = Z4(B -> denominator);
+    
+    integer *first  = Z8(A -> numerator, B_int_denom);  // Первый операнд
+    integer *second = Z8(B -> numerator, A_int_denom);  // Второй операнд
+    
+    free_integer(A_int_denom);
+    free_integer(B_int_denom);
+    
+    integer *numerator_sum = Z7(first, second); // Вычитание
+    
+    free_integer(first);
+    free_integer(second);
+    
+    natural *common_denom = N8(A -> denominator, B -> denominator); // Общий знаменатель
+    
+    fraction *C = init_fraction(numerator_sum, common_denom);       // Преобразование в дробь
+    
+    free_natural(common_denom);
+    
+    fraction *result = Q1(C);   // Сокращение полученной дроби
+    
+    free_fraction(C);
+    
+    return result;
 }
 
