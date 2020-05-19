ee3da32ef6c27e67ff2587066c1b21473670a458
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-19 15:28:05 +0300

    Q-4 updated

--- modules/q/4.h
+++ modules/q/4.h
@@ -1,13 +1,23 @@
-//Игнатьев Константин 9305
-//Преобразование дробного в целое
-integer *Q4( fraction *A) {
-        integer *R=NULL;
-        //test for the ability to transform into integer
-        if ((( A -> denominator -> length ) == 1) && (( A -> denominator -> digits[0] ) == 1)){
-            R = copy_integer(A -> numerator);
-        }
-        else{
-            R = init_integer(0);
-        }
+// Игнатьев Константин 9305
+// Преобразование дробного в целое
+
+integer *Q4(fraction *A) {
+    
+    integer *R;
+    
+    // Сокращение дроби
+    
+    fraction *reduced = Q1(A);
+    
+    // Проверка на целое
+    
+    if(reduced -> denominator -> length == 1 && reduced -> denominator -> digits[0] == 1) {
+        
+        R = copy_integer(reduced -> numerator);
+        
+    } else R = init_integer(0);
+    
+    free_fraction(reduced);
+    
     return R;
 }
