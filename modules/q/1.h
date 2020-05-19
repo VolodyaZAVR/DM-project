78f0f92ee506140e32b9c6a7ec00d6a4889ce47a
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-19 14:32:44 +0300

    Q-1 updated

--- modules/q/1.h
+++ modules/q/1.h
@@ -1,35 +1,27 @@
-//Лялина Анастасия 9307
-//Сокращение дроби
+// Лялина Анастасия 9307
+// Сокращение дроби
+
 fraction *Q1(fraction *A)
 {
-    fraction *res = copy_fraction(A);
-    natural *B = NULL;
-    int sign = 1;
-    integer *temp1=NULL;
-    natural* temp_numerator = Z1(A -> numerator), * temp_denominator = res -> denominator , *temp2 = NULL;
-    B = N13(A -> denominator, temp_numerator);//Нод числителя и знаменателя
-
-    free_natural(temp_numerator);
-    temp2 = Z1(A->numerator);
-    sign = Z2(A->numerator);
-    temp1 = res->numerator;
-    temp_numerator = N11(temp2, B);//Деление числителя на НОД
-
-    res->numerator=Z4(temp_numerator);
-
-    free_natural(temp_numerator);
-    free_integer(temp1);
-    free_natural(temp2);
-
-    res -> denominator = N11(A -> denominator, B);//Деление знаменателя на НОД
-
-    if (sign==1)
-    {
-        res->numerator->sign=false;
-
-    }
-
-    free_natural(temp_denominator);
-    free_natural(B);
-    return res;
+    natural *P = Z1(A -> numerator);
+    natural *Q = A -> denominator;
+    
+    natural *GCD = N13(P, Q);
+    
+    natural *P_divided = N11(P, GCD);
+    natural *Q_divided = N11(Q, GCD);
+    
+    free_natural(P);
+    
+    integer *numerator = Z4(P_divided);
+    
+    if(A -> numerator -> sign == false) numerator -> sign = false;
+    
+    fraction *result = init_fraction(numerator, Q_divided);
+    
+    free_integer(numerator);
+    free_natural(P_divided);
+    free_natural(Q_divided);
+    
+    return result;
 }
