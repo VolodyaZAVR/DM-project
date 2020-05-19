9a2e3573a27a866f5590c04ab190a84c003de39c
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-19 15:51:23 +0300

    Q-1 zero numbers handler added

--- modules/q/1.h
+++ modules/q/1.h
@@ -3,25 +3,43 @@
 
 fraction *Q1(fraction *A)
 {
-    natural *P = Z1(A -> numerator);
-    natural *Q = A -> denominator;
+    fraction *result;
     
-    natural *GCD = N13(P, Q);
+    // Проверка на нулевой числитель
     
-    natural *P_divided = N11(P, GCD);
-    natural *Q_divided = N11(Q, GCD);
+    if(Z2(A -> numerator) == 0) {
+        
+        // Инициализация единичного знаменателя
+        
+        natural *denom = init_natural(1);
+        denom -> digits[0] = 1;
+        
+        result = init_fraction(A -> numerator, denom);
+        
+        free_natural(denom);
+        
+    } else {
     
-    free_natural(P);
-    
-    integer *numerator = Z4(P_divided);
-    
-    if(A -> numerator -> sign == false) numerator -> sign = false;
-    
-    fraction *result = init_fraction(numerator, Q_divided);
-    
-    free_integer(numerator);
-    free_natural(P_divided);
-    free_natural(Q_divided);
+        natural *P = Z1(A -> numerator);
+        natural *Q = A -> denominator;
+        
+        natural *GCD = N13(P, Q);
+        
+        natural *P_divided = N11(P, GCD);
+        natural *Q_divided = N11(Q, GCD);
+        
+        free_natural(P);
+        
+        integer *numerator = Z4(P_divided);
+        
+        if(A -> numerator -> sign == false) numerator -> sign = false;
+        
+        result = init_fraction(numerator, Q_divided);
+        
+        free_integer(numerator);
+        free_natural(P_divided);
+        free_natural(Q_divided);
+    }
     
     return result;
 }
