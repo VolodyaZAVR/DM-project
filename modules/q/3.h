f8002a52dc075563f8a70369eb0551c90056baff
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-19 15:08:35 +0300

    Q-3 minor fixes

--- modules/q/3.h
+++ modules/q/3.h
@@ -1,12 +1,15 @@
-//Кацер Евгений 9307
-//Преобразование целого в дробное
+// Кацер Евгений 9307
+// Преобразование целого в дробное
+
 fraction *Q3(integer *A) {
-        natural *B = init_natural(1);
-        B -> digits[0] = 1;
-        // conversion to rational
-        fraction *R;
-        R = init_fraction(A, B);
-        free_natural(B);
-        
+    
+    natural *B = init_natural(1);
+    B -> digits[0] = 1;
+    
+    // Преобразование в дробь
+    
+    fraction *R = init_fraction(A, B);
+    free_natural(B);
+    
     return R;
 }
