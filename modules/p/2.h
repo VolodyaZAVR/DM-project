9c0358d9bdfd9b0bb86a105f285a07d892cdd654
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-20 22:08:30 +0300

    P-2 updated

--- modules/p/2.h
+++ modules/p/2.h
@@ -1,21 +1,24 @@
-//Салауров Евгений 9305
-//Вычитание полиномов
-polynomial *P2(polynomial *A, polynomial *B)// модуль вычитания двух многочленов
-{
-  size_t i = 0;//переменная для прохождению по массиву
-  polynomial *C = NULL;
+// Салауров Евгений 9305
+// Вычитание полиномов
 
-  if(A -> degree > B -> degree){//определяем степень какого многочлена больше
-    C = copy_polynomial(A);
-    for(i = 0; i < B -> degree+1; i++){
-      C -> factors[i] = Q6(A -> factors[i], B -> factors[i]);//вызов функции вычитания рациональных дробей
+polynomial *P2(polynomial *A, polynomial *B)
+{
+    size_t i, deg;
+    polynomial *C;
+    
+    if(A -> degree > B -> degree) {
+        
+        C = copy_polynomial(A);
+        deg = B -> degree;
+        
+    } else {
+        
+        C = copy_polynomial(B);
+        deg = A -> degree;
     }
-  }
-  else{//все действия происходят аналогично но многочлены меняются местами
-    C = copy_polynomial(B);
-    for(i = 0; i < A -> degree+1; i++){
-      C -> factors[i] = Q6(B -> factors[i], A -> factors[i]);//вызов функции вычитания рациональных дробей
-    }
-  }
-  return C;
+    
+    for(i = 0; i <= deg; i++)
+        C -> factors[i] = Q6(A -> factors[i], B -> factors[i]);
+    
+    return C;
 }
