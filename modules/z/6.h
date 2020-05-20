1c357a9e344598a51344303425fc4b26b91607ca
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-20 21:45:55 +0300

    Z-6 updated

--- modules/z/6.h
+++ modules/z/6.h
@@ -1,67 +1,74 @@
-//Когутенко Андрей 9305
-//Сложение целых чисел
+// Когутенко Андрей 9305
+// Сложение целых чисел
+
+#include <stdbool.h>
+
 integer *Z6(integer *A, integer *B)
 {
-    integer *R;
-    natural *aN, *bN, *L;
-    aN = Z1(A);
-    bN = Z1(B);
-    R = init_integer(aN -> length);
-    R = init_integer(bN -> length);
-    R = NULL;
-
-    if ((Z2(A) == 2) && (Z2(B) == 2)) {//Рассмотрение всех возможных случаев сложения
-        L = N4(aN, bN);
-        R = init_integer(L -> length);
-        R = Z4(L);
-        R -> sign = true;
+    integer *C;
+    
+    natural *A_natural = Z1(A);
+    natural *B_natural = Z1(B);
+    natural *C_natural;
+    
+    bool sign;
+    
+    if(A -> sign == B -> sign) {
+        
+        // Суммируем абсолютные величины
+        
+        C_natural = N4(A_natural, B_natural);
+        sign = A -> sign;
+        
+    } else {
+        
+        if(A -> sign == true) {
+            
+            // +-
+            
+            if(N1(A_natural, B_natural) == 1) {
+                
+                // |a| < |b|
+                
+                C_natural = N5(B_natural, A_natural);
+                sign = false;
+                
+            } else {
+                
+                // |a| >= |b|
+                
+                C_natural = N5(A_natural, B_natural);
+                sign = true;
+            }
+            
+        } else {
+            
+            // -+
+            
+            if(N1(A_natural, B_natural) == 2) {
+                
+                // |a| > |b|
+                
+                C_natural = N5(A_natural, B_natural);
+                sign = false;
+                
+            } else {
+                
+                // |a| <= |b|
+                
+                C_natural = N5(B_natural, A_natural);
+                sign = true;
+            }
+        }
     }
-    else if ((Z2(A) == 1) && (Z2(B) == 1)) {
-        L = N4(aN, bN);
-        R = init_integer(L -> length);
-        R = Z4(L);
-        R -> sign = false;
-    }
-    else if ((Z2(A) == 2) && (Z2(B) == 1)) {
-        if ((N1(aN, bN) == 2) || (N1(aN, bN) == 0)) {
-            R = Z4(N5(aN, bN));
-            R -> sign = true;
-        }
-        else if (N1(aN, bN) == 1) {
-            R = Z4(N5(bN, aN));
-            R -> sign = false;
-        }
-    }
-    else if ((Z2(A) == 1) && (Z2(B) == 2)) {
-        if (N1(aN, bN) == 2) {
-            R = Z4(N5(aN, bN));
-            R -> sign = false;
-        }
-        else if ((N1(aN, bN) == 1) || (N1(aN, bN) == 0)) {
-            R = Z4(N5(bN, aN));
-            R -> sign = true;
-        }
-    }
-    else if (Z2(A) == 0) {
-        if (Z2(B) == 2) {
-            B -> sign = true;
-        }
-        else if (Z2(B) == 1) {
-            B -> sign = false;
-        }
-        R = init_integer(B -> length);
-        R = B;
-    }
-    else if (Z2(B) == 0) {
-        if (Z2(A) == 2) {
-            A -> sign = true;
-        }
-        else if (Z2(A) == 1) {
-            A -> sign = false;
-        }
-        R = init_integer(A -> length);
-        R = A;
-    }
-    return(R);
+        
+    C = Z4(C_natural);
+    C -> sign = sign;
+    
+    free_natural(A_natural);
+    free_natural(B_natural);
+    free_natural(C_natural);
+    
+    return C;
 }
 
