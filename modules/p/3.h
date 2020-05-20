34cdaaffd60d2f4d340a3d144729af6a4cb473bc
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-20 22:10:55 +0300

    P-3 minor fixes

--- modules/p/3.h
+++ modules/p/3.h
@@ -1,15 +1,14 @@
-//Гришин Илья 9305
-//Умножение многочлена на рациональное число
+// Гришин Илья 9305
+// Умножение многочлена на рациональное число
+
 polynomial *P3(polynomial *A, fraction *B)
 {
-	polynomial *res = NULL;
-	fraction *temp = NULL;
-	
-	res = copy_polynomial(A);
+	polynomial *res = copy_polynomial(A);
+	fraction *temp;
 	
 	for (size_t i = 0; i <= A -> degree; i++)
 	{
-		temp = Q7 (A -> factors[i], B);
+		temp = Q7(A -> factors[i], B);
         
 		free_fraction(res -> factors[i]);
         
