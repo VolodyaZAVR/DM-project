e4697c922e9880ffd43a86050d047a2381c00936
Кто-то <29797602+kogutenko@users.noreply.github.com>
2020-05-20 22:16:41 +0300

    P-5 minor fixes

--- modules/p/5.h
+++ modules/p/5.h
@@ -1,8 +1,8 @@
-//Чугуй Владимир 9305
-//Старший коэффициент многочлена
+// Чугуй Владимир 9305
+// Старший коэффициент многочлена
+
 fraction *P5(polynomial *A)
 {
-	fraction *res = NULL;
-	res = copy_fraction(A -> factors[A -> degree]);
+	fraction *res = copy_fraction(A -> factors[A -> degree]);
 	return res;
 }
